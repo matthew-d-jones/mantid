291794139ec204f4a6f044a6d3f125203ed90a6c
David Fairbrother <DavidFair@users.noreply.github.com>
2020-04-20 18:26:20 +0100

    Convert boost::shared_ptr to std in AlgProperty
    Converts some boost::shared_ptr instances to std::shared_ptr so we don't
    regress this back. Adds some missing headers for those too

--- Framework/API/inc/MantidAPI/AlgorithmProperty.h
+++ Framework/API/inc/MantidAPI/AlgorithmProperty.h
@@ -10,6 +10,8 @@
 #include "MantidKernel/NullValidator.h"
 #include "MantidKernel/PropertyWithValue.h"
 
+#include <memory>
+
 namespace Mantid {
 namespace API {
 //-------------------------------------------------------------------------
@@ -34,10 +36,10 @@ API::IAlgorithm interface.
 @date 24/03/2011
 */
 class MANTID_API_DLL AlgorithmProperty
-    : public Kernel::PropertyWithValue<boost::shared_ptr<IAlgorithm>> {
+    : public Kernel::PropertyWithValue<std::shared_ptr<IAlgorithm>> {
 public:
   /// Typedef the held type
-  using HeldType = boost::shared_ptr<IAlgorithm>;
+  using HeldType = std::shared_ptr<IAlgorithm>;
 
   /// Constructor
   AlgorithmProperty(const std::string &propName,
